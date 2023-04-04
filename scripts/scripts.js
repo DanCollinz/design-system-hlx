b2df4927e52cf9d407a2a842ea9379d51904ed3f
dan <dan.collins@gmail.com>
2023-04-03 21:00:42 -0400

    banner still in nav tools

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -114,7 +114,7 @@ async function loadLazy(doc) {
 
   loadHeader(doc.querySelector('header'));
   loadFooter(doc.querySelector('footer'));
-
+  
   loadCSS(`${window.hlx.codeBasePath}/styles/lazy-styles.css`);
   addFavIcon(`${window.hlx.codeBasePath}/styles/favicon.svg`);
   sampleRUM('lazy');
